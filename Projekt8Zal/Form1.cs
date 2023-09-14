5f30d59754a69b5424d57f9811f1dd5a767d58d3
Mateusz Roszczyk <masterortografii@gmail.com>
2023-09-14 14:59:23 +0200

    Dodaj pliki projektów.

---
+++ Projekt8Zal/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace Projektowaniewizualne7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public string get_seq()
+        {
+            return textBox1.Text;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form2 frm2 = new Form2();
+            frm2.ShowDialog();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            string input = textBox1.Text;
+
+            // Sprawdź, czy cały tekst zawiera tylko litery A, C, T i G
+            if (!Regex.IsMatch(input, "^[ACTG]*$"))
+            {
+                // Jeżeli tekst zawiera inne znaki, usuń ostatnio wprowadzone
+                // znaki, które nie pasują do wzorca
+                int cursorPosition = textBox1.SelectionStart;
+                textBox1.Text = Regex.Replace(input, "[^ACTG]", string.Empty);
+                textBox1.SelectionStart = cursorPosition - 1; // Przywróć pozycję kursora
+
+                MessageBox.Show("Możesz używać tylko liter A, C, T i G.");
+            }
+        }
+    }
+}
---
+++ Projekt8Zal/Form2.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Numerics;
+using System.Text.RegularExpressions;
+using System.IO;
+
+namespace Projektowaniewizualne7
+{
+    public partial class Form2 : Form
+    {
+        List<string> seqs = new List<string>();
+        public Form2()
+        {
+            InitializeComponent();
+            Count();
+        }
+
+        public void Count()
+        {
+            string sequence = (Application.OpenForms.OfType<Form1>().FirstOrDefault()).get_seq();
+            for(int i = 0; i < sequence.Length - 3; i++)
+            {
+                string littleseq = sequence.Substring(i, 4);
+                if (!seqs.Contains(littleseq))
+                {
+                    seqs.Add(littleseq);
+                    int occurances = Regex.Matches(sequence, littleseq).Count;
+
+                    int rowId = dataGridView1.Rows.Add();
+                    DataGridViewRow row = dataGridView1.Rows[rowId];
+                    row.Cells["Column1"].Value = littleseq;
+                    row.Cells["Column2"].Value = occurances;
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (TextWriter tw = new StreamWriter("data.csv"))
+            {
+                for (int i = 0; i < dataGridView1.Rows.Count; i++)
+                {
+                    for (int j = 0; j < dataGridView1.Columns.Count; j++)
+                    {
+                        tw.Write($"{dataGridView1.Rows[i].Cells[j].Value.ToString()}");
+
+                        if (!(j == dataGridView1.Columns.Count - 1))
+                        {
+                            tw.Write(",");
+                        }
+                    }
+                    tw.WriteLine();
+                }
+            }
+        }
+    }
+}
